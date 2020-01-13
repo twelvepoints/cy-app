65ff87dbb81fe3884a318048f74486b0fdd87ff2
twelvepoints <dave.perry@originalobjective.com>
2020-01-13 18:19:37 +0000

    stuff

--- src/js/app.js
+++ src/js/app.js
@@ -27,174 +27,186 @@ window.addEventListener("load", function () {
         }
     });
 });
-  
+
 
 window.addEventListener("load", function () {
-  let cardSlider = tns({
-      container: '.card-slider',
-      loop: false,
-      mouseDrag: true,
-      swipeAngle: 35,
-      autoplay: false,
-      controls: false,
-      nav: false,
-      preventScrollOnTouch: true,
-      responsive: {
-          0: {
-              edgePadding: 24,
-              gutter: 8,
-              items: 2
-          },
-          768: {
-              edgePadding: 24,
-              gutter: 12,
-              items: 3
-          },
-          1024: {
-              edgePadding: 32,
-              gutter: 16,
-              items: 5
-          }
-      }
-  });
+    let cardSlider = tns({
+        container: '.card-slider',
+        loop: false,
+        mouseDrag: true,
+        swipeAngle: 35,
+        autoplay: false,
+        controls: false,
+        nav: false,
+        preventScrollOnTouch: true,
+        responsive: {
+            0: {
+                edgePadding: 24,
+                gutter: 8,
+                items: 2
+            },
+            768: {
+                edgePadding: 24,
+                gutter: 12,
+                items: 3
+            },
+            1024: {
+                edgePadding: 32,
+                gutter: 16,
+                items: 5
+            }
+        }
+    });
 
-  let cardSlider2 = tns({
-      container: '.card-slider-2',
-      loop: false,
-      mouseDrag: true,
-      swipeAngle: 35,
-      autoplay: false,
-      controls: false,
-      nav: false,
-      preventScrollOnTouch: true,
-      responsive: {
-          0: {
-              edgePadding: 24,
-              gutter: 8,
-              items: 2
-          },
-          768: {
-              edgePadding: 24,
-              gutter: 12,
-              items: 3
-          },
-          1024: {
-              edgePadding: 32,
-              gutter: 16,
-              items: 5
-          }
-      }
-  });
+    let cardSlider2 = tns({
+        container: '.card-slider-2',
+        loop: false,
+        mouseDrag: true,
+        swipeAngle: 35,
+        autoplay: false,
+        controls: false,
+        nav: false,
+        preventScrollOnTouch: true,
+        responsive: {
+            0: {
+                edgePadding: 24,
+                gutter: 8,
+                items: 2
+            },
+            768: {
+                edgePadding: 24,
+                gutter: 12,
+                items: 3
+            },
+            1024: {
+                edgePadding: 32,
+                gutter: 16,
+                items: 5
+            }
+        }
+    });
 
-  let cardSlider3 = tns({
-      container: '.card-slider-3',
-      loop: false,
-      mouseDrag: true,
-      swipeAngle: 35,
-      autoplay: false,
-      controls: false,
-      nav: false,
-      preventScrollOnTouch: true,
-      responsive: {
-          0: {
-              edgePadding: 24,
-              gutter: 8,
-              items: 2
-          },
-          768: {
-              edgePadding: 24,
-              gutter: 12,
-              items: 3
-          },
-          1024: {
-              edgePadding: 32,
-              gutter: 16,
-              items: 5
-          }
-      }
-  });
+    let cardSlider3 = tns({
+        container: '.card-slider-3',
+        loop: false,
+        mouseDrag: true,
+        swipeAngle: 35,
+        autoplay: false,
+        controls: false,
+        nav: false,
+        preventScrollOnTouch: true,
+        responsive: {
+            0: {
+                edgePadding: 24,
+                gutter: 8,
+                items: 2
+            },
+            768: {
+                edgePadding: 24,
+                gutter: 12,
+                items: 3
+            },
+            1024: {
+                edgePadding: 32,
+                gutter: 16,
+                items: 5
+            }
+        }
+    });
 
-  let cardSlider4 = tns({
-      container: '.card-slider-4',
-      loop: false,
-      mouseDrag: true,
-      swipeAngle: 35,
-      autoplay: false,
-      controls: false,
-      nav: false,
-      preventScrollOnTouch: true,
-      responsive: {
-          0: {
-              edgePadding: 24,
-              gutter: 8,
-              items: 2
-          },
-          768: {
-              edgePadding: 24,
-              gutter: 12,
-              items: 3
-          },
-          1024: {
-              edgePadding: 32,
-              gutter: 16,
-              items: 5
-          }
-      }
-  });
+    let cardSlider4 = tns({
+        container: '.card-slider-4',
+        loop: false,
+        mouseDrag: true,
+        swipeAngle: 35,
+        autoplay: false,
+        controls: false,
+        nav: false,
+        preventScrollOnTouch: true,
+        responsive: {
+            0: {
+                edgePadding: 24,
+                gutter: 8,
+                items: 2
+            },
+            768: {
+                edgePadding: 24,
+                gutter: 12,
+                items: 3
+            },
+            1024: {
+                edgePadding: 32,
+                gutter: 16,
+                items: 5
+            }
+        }
+    });
 
-  let cardSlider5 = tns({
-      container: '.card-slider-5',
-      loop: false,
-      mouseDrag: true,
-      swipeAngle: 35,
-      autoplay: false,
-      autoplayButton: false,
-      autoplayButtonOutput: false,
-      controls: false,
-      nav: false,
-      preventScrollOnTouch: true,
-      responsive: {
-          0: {
-              edgePadding: 24,
-              gutter: 8,
-              items: 2
-          },
-          768: {
-              edgePadding: 24,
-              gutter: 12,
-              items: 3
-          },
-          1024: {
-              edgePadding: 32,
-              gutter: 16,
-              items: 5
-          }
-      }
-  });
+    let cardSlider5 = tns({
+        container: '.card-slider-5',
+        loop: false,
+        mouseDrag: true,
+        swipeAngle: 35,
+        autoplay: false,
+        autoplayButton: false,
+        autoplayButtonOutput: false,
+        controls: false,
+        nav: false,
+        preventScrollOnTouch: true,
+        responsive: {
+            0: {
+                edgePadding: 24,
+                gutter: 8,
+                items: 2
+            },
+            768: {
+                edgePadding: 24,
+                gutter: 12,
+                items: 3
+            },
+            1024: {
+                edgePadding: 32,
+                gutter: 16,
+                items: 5
+            }
+        }
+    });
 });
 
 
 
+
+
 window.addEventListener("load", function () {
-  // store tabs variable
-  var myTabs = document.querySelectorAll("ul.nav-tabs > li");
-  function myTabClicks(tabClickEvent) {
-      for (var i = 0; i < myTabs.length; i++) {
-          myTabs[i].classList.remove("active");
-      }
-      var clickedTab = tabClickEvent.currentTarget;
-      clickedTab.classList.add("active");
-      tabClickEvent.preventDefault();
-      var myContentPanes = document.querySelectorAll(".tab-pane");
-      for (i = 0; i < myContentPanes.length; i++) {
-          myContentPanes[i].classList.remove("active");
-      }
-      var anchorReference = tabClickEvent.target;
-      var activePaneId = anchorReference.getAttribute("href");
-      var activePane = document.querySelector(activePaneId);
-      activePane.classList.add("active");
-  }
-  for (i = 0; i < myTabs.length; i++) {
-      myTabs[i].addEventListener("click", myTabClicks)
-  }
+    $(".search-open").click(function () {
+        $(".advanced-search").toggleClass("toggled")
+    }),
+        $(".search-close").click(function () {
+            $(".advanced-search").toggleClass("toggled")
+        })
+});
+
+
+window.addEventListener("load", function () {
+    // store tabs variable
+    var myTabs = document.querySelectorAll("ul.nav-tabs > li");
+    function myTabClicks(tabClickEvent) {
+        for (var i = 0; i < myTabs.length; i++) {
+            myTabs[i].classList.remove("active");
+        }
+        var clickedTab = tabClickEvent.currentTarget;
+        clickedTab.classList.add("active");
+        tabClickEvent.preventDefault();
+        var myContentPanes = document.querySelectorAll(".tab-pane");
+        for (i = 0; i < myContentPanes.length; i++) {
+            myContentPanes[i].classList.remove("active");
+        }
+        var anchorReference = tabClickEvent.target;
+        var activePaneId = anchorReference.getAttribute("href");
+        var activePane = document.querySelector(activePaneId);
+        activePane.classList.add("active");
+    }
+    for (i = 0; i < myTabs.length; i++) {
+        myTabs[i].addEventListener("click", myTabClicks)
+    }
 });
