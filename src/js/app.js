9b7708f404bca85935c5f0cb1ee75b313e7af27f
twelvepoints <dave.perry@originalobjective.com>
2020-01-19 22:21:28 +0000

    changed sliders from tiny js to owl carousel 2

--- src/js/app.js
+++ src/js/app.js
@@ -1,180 +1,54 @@
-window.addEventListener("load", function () {
-    let featuredSlider = tns({
-        container: '.featured-slider',
-        loop: false,
-        mouseDrag: true,
-        swipeAngle: 35,
-        autoplay: false,
-        controls: false,
-        nav: false,
-        preventScrollOnTouch: true,
-        responsive: {
-            0: {
-                edgePadding: 24,
-                gutter: 8,
-                items: 1
-            },
-            768: {
-                edgePadding: 24,
-                gutter: 12,
-                items: 1
-            },
-            1024: {
-                edgePadding: 32,
-                gutter: 16,
-                items: 2.99
-            }
+$('.owl-carousel-card').owlCarousel({
+    lazyLoad: true,
+    stagePadding: 24,
+    loop: false,
+    margin: 10,
+    nav: false,
+    responsive:{
+        0: {
+            stagePadding: 24,
+            margin: 8,
+            items: 2
+        },
+        768: {
+            stagePadding: 24,
+            margin: 12,
+            items: 3
+        },
+        1024: {
+            stagePadding: 32,
+            margin: 16,
+            items: 5
         }
-    });
-});
+    }
+})
 
 
-window.addEventListener("load", function () {
-    let cardSlider = tns({
-        container: '.card-slider',
-        loop: false,
-        mouseDrag: true,
-        swipeAngle: 35,
-        autoplay: false,
-        controls: false,
-        nav: false,
-        preventScrollOnTouch: true,
-        responsive: {
-            0: {
-                edgePadding: 24,
-                gutter: 8,
-                items: 2
-            },
-            768: {
-                edgePadding: 24,
-                gutter: 12,
-                items: 3
-            },
-            1024: {
-                edgePadding: 32,
-                gutter: 16,
-                items: 5
-            }
+
+$('.owl-carousel-featured').owlCarousel({
+    lazyLoad: true,
+    stagePadding: 24,
+    loop: false,
+    margin: 10,
+    nav: false,
+    responsive: {
+        0: {
+            stagePadding: 24,
+            margin: 8,
+            items: 1
+        },
+        768: {
+            stagePadding: 24,
+            margin: 12,
+            items: 1
+        },
+        1024: {
+            stagePadding: 32,
+            margin: 16,
+            items: 2.99
         }
-    });
-
-    let cardSlider2 = tns({
-        container: '.card-slider-2',
-        loop: false,
-        mouseDrag: true,
-        swipeAngle: 35,
-        autoplay: false,
-        controls: false,
-        nav: false,
-        preventScrollOnTouch: true,
-        responsive: {
-            0: {
-                edgePadding: 24,
-                gutter: 8,
-                items: 2
-            },
-            768: {
-                edgePadding: 24,
-                gutter: 12,
-                items: 3
-            },
-            1024: {
-                edgePadding: 32,
-                gutter: 16,
-                items: 5
-            }
-        }
-    });
-
-    let cardSlider3 = tns({
-        container: '.card-slider-3',
-        loop: false,
-        mouseDrag: true,
-        swipeAngle: 35,
-        autoplay: false,
-        controls: false,
-        nav: false,
-        preventScrollOnTouch: true,
-        responsive: {
-            0: {
-                edgePadding: 24,
-                gutter: 8,
-                items: 2
-            },
-            768: {
-                edgePadding: 24,
-                gutter: 12,
-                items: 3
-            },
-            1024: {
-                edgePadding: 32,
-                gutter: 16,
-                items: 5
-            }
-        }
-    });
-
-    let cardSlider4 = tns({
-        container: '.card-slider-4',
-        loop: false,
-        mouseDrag: true,
-        swipeAngle: 35,
-        autoplay: false,
-        controls: false,
-        nav: false,
-        preventScrollOnTouch: true,
-        responsive: {
-            0: {
-                edgePadding: 24,
-                gutter: 8,
-                items: 2
-            },
-            768: {
-                edgePadding: 24,
-                gutter: 12,
-                items: 3
-            },
-            1024: {
-                edgePadding: 32,
-                gutter: 16,
-                items: 5
-            }
-        }
-    });
-
-    let cardSlider5 = tns({
-        container: '.card-slider-5',
-        loop: false,
-        mouseDrag: true,
-        swipeAngle: 35,
-        autoplay: false,
-        autoplayButton: false,
-        autoplayButtonOutput: false,
-        controls: false,
-        nav: false,
-        preventScrollOnTouch: true,
-        responsive: {
-            0: {
-                edgePadding: 24,
-                gutter: 8,
-                items: 2
-            },
-            768: {
-                edgePadding: 24,
-                gutter: 12,
-                items: 3
-            },
-            1024: {
-                edgePadding: 32,
-                gutter: 16,
-                items: 5
-            }
-        }
-    });
-});
-
-
-
+    }
+})
 
 
 window.addEventListener("load", function () {
